a6c1feddc4554db8680111de2a1a7bd73a119e0f
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-09 16:30:45 -0400

    Creado endpoint GET getAllPersons()

--- src/main/java/com/example/personback/controller/PersonController.java
+++ src/main/java/com/example/personback/controller/PersonController.java
@@ -2,8 +2,10 @@ package com.example.personback.controller;
 
 import com.example.personback.entity.Person;
 import com.example.personback.service.PersonService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,10 @@ public class PersonController {
   @PostMapping
   public Person createOrUpdatePerson (@RequestBody Person person) {
     return personService.createOrUpdatePerson(person);
+  }
+
+  @GetMapping
+  public List<Person> getAllPersons() {
+    return personService.getAllPersons();
   }
 }
