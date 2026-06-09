11ece46bf9edf7fe6b1cfdd1a76fb6f699672cd9
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-09 15:15:19 -0400

    Primer commit

---
+++ src/main/java/com/example/personback/PersonBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.personback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PersonBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/personback/PersonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
