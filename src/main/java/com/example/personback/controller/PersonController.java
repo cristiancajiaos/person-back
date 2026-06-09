acf5dd7c5aae225a051358b1ebb4ead0fa7c3183
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-09 16:39:13 -0400

    Creado endpoint DELETE deletePersonById

--- src/main/java/com/example/personback/controller/PersonController.java
+++ src/main/java/com/example/personback/controller/PersonController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,5 +35,10 @@ public class PersonController {
   @GetMapping("{id}")
   public Optional<Person> getPersonById(@PathVariable("id") Long id) {
     return personService.getPersonById(id);
+  }
+
+  @DeleteMapping("{id}")
+  public void deletePersonById(@PathVariable("id") Long id) {
+    personService.deletePersonById((id));
   }
 }
