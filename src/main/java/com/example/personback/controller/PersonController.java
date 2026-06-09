663376ecba82460d65591d6af625c2ee3cdcd4d8
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-09 16:36:11 -0400

    Creado endpoint GET getPersonById

--- src/main/java/com/example/personback/controller/PersonController.java
+++ src/main/java/com/example/personback/controller/PersonController.java
@@ -3,9 +3,11 @@ package com.example.personback.controller;
 import com.example.personback.entity.Person;
 import com.example.personback.service.PersonService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,10 @@ public class PersonController {
   @GetMapping
   public List<Person> getAllPersons() {
     return personService.getAllPersons();
+  }
+
+  @GetMapping("{id}")
+  public Optional<Person> getPersonById(@PathVariable("id") Long id) {
+    return personService.getPersonById(id);
   }
 }
