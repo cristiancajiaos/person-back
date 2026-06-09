5ca80ff0d7585bd40145a44cc74c59cace3ae199
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-09 16:14:24 -0400

    Creada Entity Person

---
+++ src/main/java/com/example/personback/entity/Person.java
@@ -0,0 +1,26 @@
+package com.example.personback.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tbl_persons")
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Long firstName;
+
+  private Long lastName;
+
+  @Column(name = "email_address", unique = true, nullable = false)
+  private Long email;
+}
